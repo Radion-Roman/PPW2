d1c88065b3d847d9b28438eb8eb7f7062089840c
Radion-Roman <71257250+Radion-Roman@users.noreply.github.com>
2022-04-07 15:03:09 +0300

    Finaly work Person

---
+++ Vue Person#2/src/PersonDetails.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="person">
+        <form>
+        <div>
+            <label for="name">Numele:</label>
+            <span>{{person.name}}</span>
+        </div>
+        <div>
+            <label for="mail">Mail:</label>
+            <span>{{person.mail}}</span>
+        </div>
+        <div>
+            <label for="nr">Telefonul:</label>
+            <span>{{person.nr}}</span>
+        </div>
+        </form>    
+    </div>
+</template>
+
+<script>
+
+    export default {  
+        props: {
+            person: {
+                type: Object,
+                required: true
+            }
+        },
+        data(){
+            return {
+                //product: {}
+            }
+        },   
+    }    
+</script>
+
+<style scoped>
+    .person {
+        width: 500px;
+        border: 1px solid black;
+        margin: 10px;
+        padding: 10px;  
+    }
+</style>
---
+++ Vue Person#2/src/PersonsList.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        <PersonItem v-for="(person, i) in persons" :key="person.id" 
+        :person="person" :index="(i+1)" @viewDetails="viewDetails"></PersonItem>
+    </div>
+</template>
+<script>
+    import PersonItem from './PersonItem.vue';
+    import _ from 'lodash';
+
+    export default {
+        data(){
+            return {
+                persons: [
+                    {
+                        id: 1,
+                        work: "Mihai Eminescu",
+                        name: "Ioan Mircea",
+                        mail: "Ioan@gmail.com",
+                        nr: "01234573"
+                    },
+                    {
+                        id: 2,
+                        work: "Grigorie Vieru",
+                        name: 'Alexandru Popescu',
+                        mail: "Alexandru@bk.ru",
+                        nr: "012345671"
+                    },
+                    {
+                        id: 3,
+                        work: "Alexandr Pushkin",
+                        name: 'Timofei Alexandrovici',
+                        mail: "Timofei@mai.ru",
+                        nr: "086745362"
+                    },
+                    {
+                        id: 4,
+                        work: "Ion Creanga",
+                        name: 'Andrei Timofeivici',
+                        mail: "Andrei@gmail.com",
+                        nr: "045643456"
+                    }                         
+                ]
+            }
+        },
+        components: {
+            PersonItem
+        },
+        methods:{
+           viewDetails(id){
+                let personToView = _.find(this.persons, {id:id});
+                this.$emit("viewDetails", personToView);
+           } 
+        }
+    }
+</script>
